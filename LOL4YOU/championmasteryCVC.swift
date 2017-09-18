39e30c15a97dd97b72f4acc26ec78b397323bf3a
Francisco Parente <root.emanuel@hotmail.com>
2017-09-17 21:44:37 -0300

    Champ Mastery

---
+++ LOL4YOU/championmasteryCVC.swift
@@ -0,0 +1,13 @@
+//
+//  championmasteryCVC.swift
+//  LOL4YOU
+//
+//  Created by Emanuel Root on 17/09/17.
+//  Copyright © 2017 Emanuel Root. All rights reserved.
+//
+
+import UIKit
+
+class championmasteryCVC: UICollectionViewCell {
+    
+}
